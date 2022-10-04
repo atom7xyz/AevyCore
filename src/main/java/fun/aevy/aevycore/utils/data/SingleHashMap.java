ac71c7c40ee94a3411cd86db7fd77994297553a0
Sorridi <reallygud@protonmail.com>
2022-10-04 16:09:44 +0200

    1.7-SNAPSHOT

---
+++ src/main/java/fun/aevy/aevycore/utils/data/SingleHashMap.java
@@ -0,0 +1,22 @@
+package fun.aevy.aevycore.utils.data;
+
+import java.util.HashMap;
+
+public class SingleHashMap<K> extends HashMap<K, K>
+{
+    public SingleHashMap()
+    {
+
+    }
+
+    public K put(K key)
+    {
+        return put(key, key);
+    }
+
+    public K putIfAbsent(K key)
+    {
+        return putIfAbsent(key, key);
+    }
+
+}
